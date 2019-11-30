aedcb371e12f0d5082ff3e4c53c48cf1403b80ff
JOSE GALDINO <jose.galdino4@fatec.sp.gov.br>
2019-11-30 13:05:39 -0300

    Instalado todas as dependencias do projeto

--- src/DNA.UI.WebDesign/Startup.cs
+++ src/DNA.UI.WebDesign/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Web.UI
+namespace DNA.UI.WebDesign
 {
     public class Startup
     {
