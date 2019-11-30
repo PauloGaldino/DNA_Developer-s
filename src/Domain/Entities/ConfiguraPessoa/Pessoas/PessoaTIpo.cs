94d9524f5b65372ea637a3dd43813134da0cd112
JOSE GALDINO <jose.galdino4@fatec.sp.gov.br>
2019-11-29 23:49:29 -0300

    Configurado a estrutura das pastas e a instalação dos projetos.

---
+++ src/Domain/Entities/ConfiguraPessoa/Pessoas/PessoaTIpo.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Entities.ConfiguraPessoa.Pessoas
+{
+    public class PessoaTIpo
+    {
+        public PessoaTIpo()
+        {
+
+        }
+        public int PessoaTipoID { get; set; }
+        public string Descricao { get; set; }
+    }
+}
