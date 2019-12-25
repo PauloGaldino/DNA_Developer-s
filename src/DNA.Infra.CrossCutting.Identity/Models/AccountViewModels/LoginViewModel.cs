45d19328cbd142dfff386581be148449c07c9b1e
JOSE GALDINO <jose.galdino4@fatec.sp.gov.br>
2019-12-25 06:24:40 -0300

    estilizado a pagina de enter register e index cliente

--- src/DNA.Infra.CrossCutting.Identity/Models/AccountViewModels/LoginViewModel.cs
+++ src/DNA.Infra.CrossCutting.Identity/Models/AccountViewModels/LoginViewModel.cs
@@ -6,13 +6,15 @@ namespace DNA.CrossCutting.Identity.Models.AccountViewModels
     {
         [Required]
         [EmailAddress]
+        [Display(Name = "E-mail ")]
         public string Email { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
+        [Display(Name = "Senha")]
         public string Password { get; set; }
 
-        [Display(Name = "Lembre de mim ? ")]
+        [Display(Name = "Lembrar desta conta? ")]
         public bool RememberMe { get; set; }
     }
 }
